1336222ac5419fa50cd36c427f8cbb3253cfefd2
Karim-Elgenidy <karimsherif2410@aucegypt.edu>
2020-09-08 12:29:14 +0200

    Second K commit

--- Project/ConsoleApplication3.cpp
+++ Project/ConsoleApplication3.cpp
@@ -5,21 +5,21 @@ using namespace std;
 
 // A utility function that returns maximum of two integers
 int max(int a, int b) { return (a > b) ? a : b; }
-int knapSack(int W, int wt[], int val[], int n)
+int Sack(int W, int wt[], int val[], int n)
 {
 	if (n == 0 || W == 0) // Base Case
 		return 0;
 	// If weight of the nth item is more than Knapsack capacity W, then this item cannot be included
 		// in the optimal solution
 		if (wt[n - 1] > W) {
-			return knapSack(W, wt, val, n - 1);
+			return Sack(W, wt, val, n - 1);
 		}
 			
 	// Return the maximum of two cases:
 	// (1) nth item included
 	// (2) not included
 		else
-			return max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1), knapSack(W, wt,
+			return max(val[n - 1] + Sack(W - wt[n - 1], wt, val, n - 1), Sack(W, wt,
 				val, n - 1));
 }
 // Driver code
@@ -29,7 +29,7 @@ int main()
 	int wt[] = { 10, 20, 30 };
 	int W = 50;
 	int n = sizeof(val) / sizeof(val[0]);
-	cout << knapSack(W, wt, val, n);
+	cout << Sack(W, wt, val, n);
 	return 0;
 }
 // This code is contributed by rathbhupendra
